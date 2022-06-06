9cf367dd0e94daaaf3345fc82c0e542f7b842415
abdoukane <abdoukane@wustl.edu>
2022-06-06 14:31:38 -0500

    Create view_post.php

---
+++ view_post.php
@@ -0,0 +1,42 @@
+<?php
+      
+      include_once('include/initialize.php'); 
+      $index = $_REQUEST['id'];
+       getHeader();
+    $post = getPost($index);
+   // var_dump($index);
+    echo"
+   
+       
+    <div class='container'> 
+        <div class = 'div1'>
+            <h2> ".$post['Title']. "</h2>
+           
+                <p> ".$post['Content']. "</p>
+
+               
+                
+
+                
+          
+          
+             <a href='index.php'>Back to main page</a>  
+        </div>          
+        </div>
+
+        
+
+
+    
+</body>
+
+</head>
+</html>
+
+    ";
+
+   
+
+    
+      ?>
+
