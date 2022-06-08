677d5cbba195332a6c922bea15b08540561ad277
abdoukane <abdoukane@wustl.edu>
2022-06-08 11:11:55 -0500

    h

---
+++ main.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title> Spendsaver </title> 
+         <link rel='stylesheet' href='main.css'>
+        <body>
+      
+
+        <div class = 'header' > 
+            <h1 class = 'title'>Spendsaver </h1>
+        </div>
+
+        <div class = 'footer'>
+            <h1 class= 'end'> Icons for different pages should be here!</h1>
+
+
+        
+        </body>
+
+    </head>
+</html> 
