96c17250b1e36734ab9d5dc61db9040d8698b7aa
abdoukane <abdoukane@wustl.edu>
2022-06-09 09:11:04 -0500

    wireframe

--- main.php
+++ main.php
@@ -8,10 +8,10 @@
         <div class = 'header' > 
             <h1 class = 'title'>Spendsaver </h1>
         </div>
-
+<footer>
         <div class = 'footer'>
             <h1 class= 'end'> Icons for different pages should be here!</h1>
-
+</footer>
 
         
         </body>
