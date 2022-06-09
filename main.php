c83bd5e2e73be7621149f344cc1a9372cd5d701d
abdoukane <abdoukane@wustl.edu>
2022-06-09 09:17:29 -0500

    i

--- main.php
+++ main.php
@@ -8,10 +8,15 @@
         <div class = 'header' > 
             <h1 class = 'title'>Spendsaver </h1>
         </div>
-<footer>
+
+        <div class= 'flex'>
+            
+
+        </div>  
+
         <div class = 'footer'>
             <h1 class= 'end'> Icons for different pages should be here!</h1>
-</footer>
+
 
         
         </body>
