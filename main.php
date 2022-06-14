af477b6c7d174f9c7326015f2baf55a4518de526
abdoukane <abdoukane@wustl.edu>
2022-06-14 15:39:58 -0500

    new

--- main.php
+++ main.php
@@ -1,28 +1,39 @@
 
-            <?php
+ <?php
 
 include_once('include/initialize.php'); 
 $getExpenses= getExpenses();
 var_dump($getExpenses);
 getHeader();
+
 echo"
 <div class= 'container'>
     <div class = 'form'>
         <h1 class = 'formtext'> FORM GOES HERE </h1>
     </div>  
-
-    <div class = 'expenselist'>
+  <div class = 'expenselist'>
+   
+  "; 
+  ?>
+  <?php
+   foreach($getExpenses as $index){
+       echo" 
+       
+    <h1 class = 'expenses'> $index[Amount]</h1>
     
-    <h1 class = 'expenses'> $getExpenses</h1>
-    
+   
     </div>
+    ";}
+    ?>
+    <?php
+    echo"
 
     <div class = 'possiblemeters'>
     <h1 class = 'meters'> METERS </h1>
     </div>
 
-</div>  
-";
+</div>  ";
+
 getFooter();
 
 
--- view_expense.php
+++ view_expense.php
@@ -2,4 +2,16 @@
 
 include_once('include/initialize.php'); 
 
+$getExpense = getExpense();
+echo"
+
+<div class= 'container'>
+
+<div class = 'expenselist'>
+    
+<h1 class = 'expenses'> $getExpenses[Amount]</h1>
+
+</div>
+
+</div>
 
