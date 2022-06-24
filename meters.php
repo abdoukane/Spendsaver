82bd52577ac4c8c4e0172d52b63d8b58ec86dcdb
abdoukane <abdoukane@wustl.edu>
2022-06-24 11:16:10 -0500

    uh

--- meters.php
+++ meters.php
@@ -1,19 +1,42 @@
 <?php
 include_once('include/initialize.php'); 
 getHeader();
+
+
+
+$meterData = [
+	123 => [
+		'monthlyBudget' => 400,
+		'currentSpend' => 250
+	],
+	567 => [
+		'monthlyBudget' => 750,
+		'currentSpend' => 100
+	]
+];
+$getExpenses= getExpenses();
 echo"
 
 
 <div class='shell'>
 <h1>CATEGORY</h1>
+    <div class='meteroutline'>
     <div class='percentfilled'>80%</div>
     </div>
+   </div>
 
 
     <style>
 
+    .meteroutline{  
+        background-color: #505050;
+        position: absolute;
+        width: 100%;
+        border-radius: 20px;
+       
+    }
 .shell{  
-    background-color: #505050;
+    background-color: #272727;
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
@@ -29,7 +52,7 @@ echo"
     padding:10px;
     text-align:right;
     border-radius: 20px;
-    width:60%;
+    width:70%;
 }
     </style>
 
