ce06c9ebf314e329db6b21db68d031cda3612f61
abdoukane <abdoukane@wustl.edu>
2022-07-07 09:28:12 -0500

    Update expense_table.php

--- expense_table.php
+++ expense_table.php
@@ -2,7 +2,46 @@
 
 include_once('include/initialize.php'); 
 
-$expenses = dbQuery(
-    " SELECT Amount FROM Expenses "
-);
+$getExpenses= getExpenses();
+//$deleteExpense = deleteExpense();
+
+
+if(isset($_REQUEST['ExpenseId'])) {
+    $ExpenseId = $_REQUEST['ExpenseId'];
+   $deleteExpense = deleteExpense($ExpenseId);
+   return $deleteExpense;
+
+    location.reload();
+}
+
+echo"
+<table>
+<tr>
+    <td>Category</td>
+    <td>Amount</td>
+    <td>Date</td>
+    <td>Delete/Edit</td>
+</tr>
+";
+
+foreach($getExpenses as $index){
+  
+     echo" 
+<tr>
+    <td>Cat name</td>
+    <td>$index[Amount]</td>
+    <td>$index[DateCreated] </td>
+    <td> 
+        <a href='?ExpenseId=$index[ExpenseId]'>delete</a>
+        <a href='#'>edit</a>
+    </td>
+</tr>
+   
+
+";
+    
+}
+
+
+
 
