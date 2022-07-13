6e366e9e51f381275bd46cd6efc6a2b0c563f028
abdoukane <abdoukane@wustl.edu>
2022-07-13 15:33:37 -0500

    deleted testmeter

--- meters.php
+++ meters.php
@@ -2,36 +2,33 @@
 include_once('include/initialize.php'); 
 getHeader();
 
-$meterData = [
-	123 => [
-        'category' => 'Food',
-		'monthlyBudget' => 400,
-		'currentSpend' => 390
-	],
-	567 => [
-        'category' => 'Necessities',
-		'monthlyBudget' => 500,
-		'currentSpend' => 100
-	]
-];
+$meterData = getExpenses();
 
+$sum = ExpenseTotal();
 echo"
 <div class = 'container'>
 <div class='shell'>
 
 ";
-foreach($meterData as $category){
-    //$cat = getCategories($CategoryId);
-   $name = $category['CategoryId'];
-$percentfilled = ($category['currentSpend']/$category['monthlyBudget'])*100;
-echo"
-    <h1 style= 'color: #FF652F;'>$name</h1>
-    <div class='meteroutline'>
-    <div class='percentfilled' style= 'width: $percentfilled%;'>$percentfilled%</div>
-    </div>
- 
-   ";
-}
+
+    foreach($sum as $total){
+        
+        $name = $total['Name'];
+    $percentfilled = ($total['Total']/$total['Budget'])*100;
+
+    echo"
+        <h1 style= 'color: #FF652F;'>$name</h1>
+        <div class='meteroutline'>
+        <div class='percentfilled' style= 'width: $percentfilled%;'>$percentfilled%</div>
+        </div>
+    
+        ";
+    
+ }
+
+
+
+
 echo" 
  </div>
  </div>
