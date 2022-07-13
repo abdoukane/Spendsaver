362d1f86d7f3e6d84cd3bacd81631b030c69c654
abdoukane <abdoukane@wustl.edu>
2022-07-13 15:31:48 -0500

    PR change

--- include/expenses.php
+++ include/expenses.php
@@ -2,37 +2,22 @@
 
 function getExpenses(){
 
-$expenses = dbQuery(
-    " 
-    SELECT *
-
-    FROM Expenses
-
-    ORDER BY DateCreated DESC
-
-    "
-)->fetchAll();
-
-return $expenses;
-
-}
-
-
-
-
-
-function getExpense(){
-
     $expenses = dbQuery(
         " 
         SELECT *
-    
         FROM Expenses
-    
-    
+        ORDER BY DateCreated DESC
+        "
+    )->fetchAll();
+    return $expenses;
+}
+
+function getExpense(){
+    $expenses = dbQuery(
+        " 
+        SELECT *
+        FROM Expenses
         "
     )->fetch();
-    
     return $expense;
-    
     }
