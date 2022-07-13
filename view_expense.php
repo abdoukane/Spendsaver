b70c043090682674135025ab2648ba0e0de0b4be
abdoukane <abdoukane@wustl.edu>
2022-07-13 15:36:02 -0500

    view expense not needed

--- view_expense.php
+++ view_expense.php
@@ -1,17 +1,1 @@
-<?php
-
-include_once('include/initialize.php'); 
-
-$getExpense = getExpense();
-echo"
-
-<div class= 'container'>
-
-<div class = 'expenselist'>
-    
-<h1 class = 'expenses'> $getExpenses[Amount]</h1>
-
-</div>
-
-</div>
 
